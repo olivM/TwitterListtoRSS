4c58c0f39fb0f3e60cd2e8fa432ee7eae5056f33
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2010-08-25 19:15:05 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterListtoRSS::Application.config.secret_token = 'e29510ed7218485c5361bebd9fa9b1af6dfb1d5f522619e351b1ae47a24d54932a5cd823bf733c1a39715c2135b4c6d9b5f123136493d2a4cc21119f895c2332'
